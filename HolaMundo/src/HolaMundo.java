824b31de7290f4e88ed22f2cda5c63aa7db6900e
ubuntu <leomtez@gmail.com>
2023-03-05 11:51:19 +0100

    1 cambio Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("Cambio 1 consola");	
+	System.out.println("Cambio 1 Eclipse");
 
 	}
 
